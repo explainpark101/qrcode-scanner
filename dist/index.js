80411632671735f8f7e369d6a007de2f114a7a24
박재형 <47944126+explainpark101@users.noreply.github.com>
2024-06-04 15:52:11 +0900

    이미지 미리보기 추가

--- dist/index.js
+++ dist/index.js
@@ -22,9 +22,16 @@ fileInput.addEventListener("input", async e=>{
     const result = await QrScanner.scanImage(fileInput.files?.[0]);
     if (/^https?:\/\/.*/.test(result)) resultDiv.innerHTML = `<a href="${result}" target="_blank">${result}</a>`;
     else resultDiv.innerHTML = result;
+
+    const imgElement = (document.querySelector(`img#image`) || document.createElement(`img`));
+    imgElement.id = 'image';
+    document.querySelector`div#image`.appendChild(imgElement);
+
+    imgElement.src = URL.createObjectURL(fileInput.files[0]);
 });
 
 window.addEventListener("paste", e=>{
     fileInput.files = e.clipboardData.files;
     fileInput.dispatchEvent(new Event('input'));
+
 });
