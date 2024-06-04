4a6e5d14abe09df245e7fa1d36e8feeb35d173b1
박재형 <47944126+explainpark101@users.noreply.github.com>
2024-06-04 15:58:53 +0900

    파일 제한 및 이미지 미리보기 추가

--- dist/index.js
+++ dist/index.js
@@ -19,15 +19,31 @@ cameraButton.addEventListener("click", e=>{
 });
 
 fileInput.addEventListener("input", async e=>{
-    const result = await QrScanner.scanImage(fileInput.files?.[0]);
-    if (/^https?:\/\/.*/.test(result)) resultDiv.innerHTML = `<a href="${result}" target="_blank">${result}</a>`;
-    else resultDiv.innerHTML = result;
+    try {
+        const result = await QrScanner.scanImage(fileInput.files?.[0]);
+        if (/^https?:\/\/.*/.test(result)) resultDiv.innerHTML = `<a href="${result}" target="_blank">${result}</a>`;
+        else resultDiv.innerHTML = result;
 
-    const imgElement = (document.querySelector(`img#image`) || document.createElement(`img`));
-    imgElement.id = 'image';
-    document.querySelector`div#image`.appendChild(imgElement);
+        const imgElement = (document.querySelector(`img#image`) || document.createElement(`img`));
+        imgElement.id = 'image';
+        document.querySelector`div#image`.appendChild(imgElement);
 
-    imgElement.src = URL.createObjectURL(fileInput.files[0]);
+        imgElement.src = URL.createObjectURL(fileInput.files[0]);
+    }
+    catch(err) {
+        if (err == 'Unsupported image type.') {
+            alert("Invalid Input. Please input the right image.");
+        }
+        else if (err == 'No QR code found') {
+            const imgElement = (document.querySelector(`img#image`) || document.createElement(`img`));
+            imgElement.id = 'image';
+            document.querySelector`div#image`.appendChild(imgElement);
+
+            imgElement.src = URL.createObjectURL(fileInput.files[0]);
+            alert("No QR code found. Please input the right image.");
+        }
+        else console.error(err);
+    }
 });
 
 window.addEventListener("paste", e=>{
